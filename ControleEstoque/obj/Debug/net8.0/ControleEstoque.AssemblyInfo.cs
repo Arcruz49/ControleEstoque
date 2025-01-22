f015d5dfb99f5a1b63bb3865f7e8e6e0781a6720
Arthur Cruz <arthurcruzdbp@gmail.com>
2025-01-22 00:57:02 -0300

    tabelas corrigidas(ids removidos)

--- ControleEstoque/obj/Debug/net8.0/ControleEstoque.AssemblyInfo.cs
+++ ControleEstoque/obj/Debug/net8.0/ControleEstoque.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ControleEstoque")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+445442a7f1d349d9467236a9a19bdb1875cf67a5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+435a438201178bac78fbdff9bbda57d73d890da9")]
 [assembly: System.Reflection.AssemblyProductAttribute("ControleEstoque")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ControleEstoque")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
