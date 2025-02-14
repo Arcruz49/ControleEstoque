8d628bdcce5c6d7f6142678c17326e3c7c8b29a8
Arthur Cruz <arthur.cruz@objetivasolucao.com.br>
2025-02-14 20:23:01 -0300

    materiais

---
+++ ControleEstoque/Controllers/MateriaisController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ControleEstoque.Controllers
+{
+    public class MateriaisController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
